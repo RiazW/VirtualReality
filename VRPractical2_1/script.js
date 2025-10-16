b89c7f9fbedbbcfe8114056002ec075d38c42884
RiazW <Riazw@nycstudents.net>
2025-10-16 01:12:01 +0000

    finish

--- VRPractical2_1/script.js
+++ VRPractical2_1/script.js
@@ -42,19 +42,19 @@ window.addEventListener("DOMContentLoaded",function() {
   
   let cloud1 = document.createElement("a-sphere");
   cloud1.setAttribute("color","white");
-  cloud1.setAttribute("position","0 1 0");
+  cloud1.setAttribute("position","0 1.5 0");
   cloud1.setAttribute("radius","1.1");
   cloud.append( cloud1 );
 
   let cloud2 = document.createElement("a-sphere");
   cloud2.setAttribute("color","white");
-  cloud2.setAttribute("position","-1 1.2 0.1");
+  cloud2.setAttribute("position","-1 1.7 0.1");
   cloud2.setAttribute("radius","1");
   cloud.append( cloud2 );
 
   let cloud3 = document.createElement("a-sphere");
   cloud3.setAttribute("color","white");
-  cloud3.setAttribute("position","1 1.3 -0.1");
+  cloud3.setAttribute("position","1 1.8 -0.1");
   cloud3.setAttribute("radius","1");
   cloud.append( cloud3 );
   cloud.setAttribute("position", {x:x, y:y, z:z} );
